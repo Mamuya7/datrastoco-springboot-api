21835d5ec4a2a41beb3a3385cb7e3c8bd53d72c5
Mamuya7 <mamuyagilbert@gmail.com>
2021-03-04 21:56:27 +0300

    CrossOrigin Configuration

--- src/main/java/com/mamuya/datrastocospringbootapi/auth/authController/AuthController.java
+++ src/main/java/com/mamuya/datrastocospringbootapi/auth/authController/AuthController.java
@@ -11,9 +11,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedHashMap;
@@ -32,6 +32,7 @@ public class AuthController {
         this.jwtTokenUtil = jwtTokenUtil;
     }
 
+    @CrossOrigin
     @PostMapping("/login")
     public ResponseEntity<LinkedHashMap<String, Object>> login(@RequestBody AuthRequest authRequest) throws Exception{
 
--- src/main/java/com/mamuya/datrastocospringbootapi/security/SecurityConfig.java
+++ src/main/java/com/mamuya/datrastocospringbootapi/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -29,8 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf()
-                .disable()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
